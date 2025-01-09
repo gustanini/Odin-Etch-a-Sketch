35b2a60ed79f28a18df3ce50ab8e2bf30990082a
Rafael Pimentel <47717581+gustanini@users.noreply.github.com>
2025-01-09 10:51:50 +0100

    feat: add board cleanup before populating, add amountOfSquares variable to hold size^2 in script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function populateBoard(size){
     // select board
     let board = document.querySelector(".board");
+    
+    // clear board before populating
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.remove());
+
     // size^2 grid
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     
+    amountOfSquares = size * size;
     // add size^2 divs, color blue
-    for (i = 0; i < (size * size); i++){
+    for (i = 0; i < amountOfSquares; i++){
         let square = document.createElement("div");
         square.style.backgroundColor = "blue";
         board.insertAdjacentElement("beforeend", square);
